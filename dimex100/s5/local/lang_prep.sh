6654da587a990b2b5fe6a2873a4ff668fe4c0978
Daniel Campoverde <alx@sillybytes.net>
2019-03-22 18:08:41 -0500

    Lang phones preparation

---
+++ dimex100/s5/local/lang_prep.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+## Only run this file from the example root directory
+##      $ ./local/data_prep.sh
+
+CORPUS_DIR="$1"
+OOV_SYMBOL="$2"
+
+mkdir -p "data/local/dict"
+
+#############################
+# data/local/dict/lexicon.txt
+#############################
+
+cp "$CORPUS_DIR/diccionarios/T22.full.dic" data/local/dict/lexicon.txt
+
+
+#######################################
+# data/local/dict/silence_phones.txt
+# data/local/dict/nonsilence_phones.txt
+# data/local/dict/extra_questions.txt
+#######################################
+
+# Use the Mexbet phones alphabet
+# http://turing.iimas.unam.mx/~luis/DIME/DIMEx100/manualdimex100/mexbet.html
+
+touch data/local/dict/silence_phones.txt
+touch data/local/dict/extra_questions.txt
+cat "$CORPUS_DIR/diccionarios/T22.full.dic" \
+    | tr '\t' ' ' \
+    | cut -d' ' -f1 --complement \
+    | sed 's/ / /g' \
+    | sort -u \
+    > data/local/dict/nonsilence_phones.txt
