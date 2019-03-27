0fd8909fbce00d7807b08bc91220d844518b4409
Daniel Campoverde <alx@sillybytes.net>
2019-03-27 18:21:27 -0500

    Filter out silence phones from non-silence phones

--- dimex100/s5/local/lang_prep.sh
+++ dimex100/s5/local/lang_prep.sh
@@ -11,7 +11,7 @@ mkdir -p "data/local/dict"
 # data/local/dict/lexicon.txt
 #############################
 
-echo -e "!SIL sil\n<UNK> spn" > data/local/dict/lexicon.txt
+echo -e '!SIL sil\n<UNK> spn' > data/local/dict/lexicon.txt
 cat "$CORPUS_DIR/diccionarios/T22.full.dic" >> data/local/dict/lexicon.txt
 
 
@@ -22,10 +22,11 @@ cat "$CORPUS_DIR/diccionarios/T22.full.dic" >> data/local/dict/lexicon.txt
 # data/local/dict/extra_questions.txt
 #######################################
 
-echo -e "sil\nspn" > data/local/dict/silence_phones.txt
-echo -e "sil" > data/local/dict/optional_silence.txt
+echo -e 'sil\nspn' > data/local/dict/silence_phones.txt
+echo -e 'sil' > data/local/dict/optional_silence.txt
 cat data/local/dict/lexicon.txt \
-    | grep -v oov \
+    | grep -v '<UNK>' \
+    | grep -v '!SIL' \
     | tr '\t' ' ' \
     | cut -d' ' -f1 --complement \
     | sed 's/ /\n/g' \
