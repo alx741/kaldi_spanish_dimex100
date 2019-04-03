9d58ff43fd1cf09324aa5befc861f709b407d1b7
Daniel Campoverde <alx@sillybytes.net>
2019-04-03 17:59:44 -0500

    Make lexicon lower case

--- dimex100/s5/local/lang_prep.sh
+++ dimex100/s5/local/lang_prep.sh
@@ -7,12 +7,14 @@ CORPUS_DIR="$1"
 
 mkdir -p "data/local/dict"
 
+source ./path.sh
+
 #############################
 # data/local/dict/lexicon.txt
 #############################
 
 echo -e '!SIL sil\n<UNK> spn' > data/local/dict/lexicon.txt
-cat "$CORPUS_DIR/diccionarios/T22.full.dic" >> data/local/dict/lexicon.txt
+cat "$CORPUS_DIR/diccionarios/T22.full.dic" | tr '[:upper:]' '[:lower:]' >> data/local/dict/lexicon.txt
 
 
 #######################################
@@ -31,4 +33,5 @@ cat data/local/dict/lexicon.txt \
     | cut -d' ' -f1 --complement \
     | sed 's/ /\n/g' \
     | sort -u \
+    | tr '[:upper:]' '[:lower:]' \
     > data/local/dict/nonsilence_phones.txt
