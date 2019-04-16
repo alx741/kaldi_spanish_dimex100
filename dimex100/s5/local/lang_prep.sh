57e52c23c72b2f9cb6e091521bcc8de0edd71181
Daniel Campoverde <alx@sillybytes.net>
2019-04-16 17:50:43 -0500

    Fix lexicon n~ substitution

--- dimex100/s5/local/lang_prep.sh
+++ dimex100/s5/local/lang_prep.sh
@@ -17,7 +17,7 @@ echo -e '!SIL sil\n<UNK> spn' > data/local/dict/lexicon.txt
 cat "$CORPUS_DIR/diccionarios/T22.full.dic" \
     | tr '[:upper:]' '[:lower:]' \
     | sed -e 's/([0123456789]*)//g' \
-        -e 's/[^ ]n\~/ñ/g' \
+        -e 's/\([^ ]\)n\~/\1ñ/g' \
         -e 's/a_7/á/g' -e 's/e_7/é/g' -e 's/i_7/í/g' -e 's/o_7/ó/g' -e 's/u_7/ú/g' \
         -e 's/a-7/á/g' -e 's/e-7/é/g' -e 's/i-7/í/g' -e 's/o-7/ó/g' -e 's/u-7/ú/g' \
         -e 's/a_/á/g' -e 's/e_/é/g' -e 's/i_/í/g' -e 's/o_/ó/g' -e 's/u_/ú/g' \
