9191de64bcf5b9bed04921a32ad50f78491c241b
Daniel Campoverde <alx@sillybytes.net>
2019-04-16 17:35:15 -0500

    Train triphones

--- dimex100/s5/run.sh
+++ dimex100/s5/run.sh
@@ -6,16 +6,21 @@
 
 train_cmd="utils/run.pl"
 decode_cmd="utils/run.pl"
+
+# TODO: break this to see if something complains
 CORPUS_DIR="CorpusDimex100"
 
 N_HMM=2000 # leaves
-N_GAUSSIANS=10000
+N_GAUSSIANS=11000
 
 
 #################
 # Download corpus
 #################
 
+echo
+echo Downloading corpus
+echo
 if [ ! -d "$CORPUS_DIR" ]; then
   wget http://turing.iimas.unam.mx/~luis/DIME/DIMEx100/DVD/DVDCorpusDimex100.zip || exit 1;
   unzip DVDCorpusDimex100.zip || exit 1;
@@ -26,6 +31,9 @@ fi
 # Data preparation
 ##################
 
+echo
+echo Data preparation
+echo
 rm -rf data exp mfcc
 local/data_prep.sh "$CORPUS_DIR"
 utils/fix_data_dir.sh "data/train"
@@ -36,6 +44,9 @@ utils/fix_data_dir.sh "data/test"
 # Features generation
 #####################
 
+echo
+echo Features generation
+echo
 steps/make_mfcc.sh --cmd "$train_cmd" "data/train" "exp/make_mfcc/train" mfcc
 steps/make_mfcc.sh --cmd "$train_cmd" "data/test"  "exp/make_mfcc/test"  mfcc
 
@@ -50,6 +61,9 @@ utils/validate_data_dir.sh "data/test"
 # Lang data preparation
 #######################
 
+echo
+echo Language data preparation
+echo
 rm -rf data/local/dict
 local/lang_prep.sh "$CORPUS_DIR"
 utils/prepare_lang.sh data/local/dict "<UNK>" data/local/lang data/lang
@@ -61,6 +75,9 @@ utils/fix_data_dir.sh "data/test"
 # Language model preparation
 ############################
 
+echo
+echo Language model preparation
+echo
 local/lm_prep.sh
 
 
@@ -68,22 +85,27 @@ local/lm_prep.sh
 # Training and Decoding
 #######################
 
-utils/subset_data_dir.sh --first data/train 500 data/train_100
+echo
+echo Training
+echo
+# utils/subset_data_dir.sh --first data/train 500 data/train_500
 
 # Training and aligning
-steps/train_mono.sh --cmd "$train_cmd" data/train_100 data/lang exp/mono || exit 1
-# steps/align_si.sh --cmd "$train_cmd" data/train data/lang exp/mono exp/mono_aligned || exit 1
-# steps/train_deltas.sh "$N_HMM" "$N_GAUSSIANS" data/train data/lang exp/mono_aligned exp/tri1 || exit 1
-# steps/align_si.sh --cmd "$train_cmd" data/train data/lang exp/tri1 exp/tri1_aligned || exit 1
+steps/train_mono.sh --cmd "$train_cmd" data/train data/lang exp/mono || exit 1
+steps/align_si.sh --cmd "$train_cmd" data/train data/lang exp/mono exp/mono_aligned || exit 1
+steps/train_deltas.sh "$N_HMM" "$N_GAUSSIANS" data/train data/lang exp/mono_aligned exp/tri1 || exit 1
+steps/align_si.sh --cmd "$train_cmd" data/train data/lang exp/tri1 exp/tri1_aligned || exit 1
+
+steps/train_lda_mllt.sh --cmd "$train_cmd" "$N_HMM" "$N_GAUSSIANS" data/train data/lang exp/tri1_aligned exp/tri2 || exit 1;
 
 # Graph compilation
-# utils/mkgraph.sh data/lang exp/tri1_aligned exp/tri1_aligned/graph_tgpr
-# utils/mkgraph.sh data/lang exp/mono exp/mono/graph_tgpr
-## this one for mono trainings
-utils/mkgraph.sh --mono data/lang exp/mono exp/mono/graph_tgpr
+utils/mkgraph.sh data/lang exp/tri2 exp/tri2/graph_tgpr
 
 # Decoding
-# steps/decode.sh --cmd "$decode_cmd" exp/tri1_aligned/graph_tgpr data/test exp/tri1_aligned/decode_test
-steps/decode.sh --cmd "$decode_cmd" exp/mono/graph_tgpr data/test exp/mono/decode_test
+echo
+echo Decoding
+echo
+steps/decode.sh --config conf/decode.config --cmd "$decode_cmd" exp/tri2/graph_tgpr data/test exp/tri2/decode_test
+# steps/decode.sh --config conf/decode.config --cmd "$decode_cmd" exp/mono/graph_tgpr data/train exp/mono/decode_test
 
 for x in exp/*/decode*; do [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh; done
